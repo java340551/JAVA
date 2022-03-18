fc6710e215092b1528f9ce6e5c51020de19e2f96
java340551 <99716561+java340551@users.noreply.github.com>
2022-03-18 20:26:44 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-    public static void main(String args[]){
-        System.out.println("Hello World!");
+public class Hello{ //一个类的声明，声明必须与文件的名字相同
+    public static void main(String args[]){ //main方法,程序从这个方法开始执行
+        System.out.println("Hello World!"); //输出Hello World! 并自动换行
     } 
 }
