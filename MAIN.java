b743db19ee26f3b7e63a00ed8d2fd4e0b107a602
java340551 <99716561+java340551@users.noreply.github.com>
2022-03-19 20:38:49 +0800

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,8 @@
+
+public class MAIN {
+    public static void main(String[] args){
+        System.out.println("Hello!"); //换行输出
+        System.out.print("Hello");
+        System.out.print(" World!"); //不换行输出
+    }
+}
